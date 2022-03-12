8a8b80a6504bd3de960d3b8b706a38971eb89443
DaanTol <686379@student.inholland.nl>
2022-03-12 16:12:07 +0100

    Added class Drink, class DrinkService, class DrinkDao

---
+++ SomerenDAL/DrinkDao.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using SomerenModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace SomerenDAL
+{
+    public class DrinkDao : BaseDao
+    {
+        public List<Drink> GetAllDrinks()
+        {
+            // select the query from the drinks database
+            string query = "SELECT drinkId, drinkName, drinkPrice, drinkStock, drinkVAT FROM [Drinks]";
+            SqlParameter[] sqlParameters = new SqlParameter[0];
+            return ReadTables(ExecuteSelectQuery(query, sqlParameters));
+        }
+
+        private List<Drink> ReadTables(DataTable dataTable)
+        {
+            List<Drink> drinks = new List<Drink>();
+
+            // check each row of the DataTable
+            foreach (DataRow dr in dataTable.Rows)
+            {
+                Drink drink = new Drink()
+                {
+                    DrinkId = (int)dr["drinkId"],
+                    DrinkName = (string)dr["..."],
+                    DrinkPrice = (double)dr["..."],
+                    DrinkStock = (int)dr["..."],
+                    DrinkVAT = (double)dr["..."],
+                };
+                // add the drink to the list
+                drinks.Add(drink);
+            }
+            return drinks;
+        }
+    }
+}
---
+++ SomerenModel/Drink.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SomerenModel
+{
+    public class Drink
+    {
+        public int DrinkId { get; set; } // DrinkId, e.g. 2
+        public string DrinkName { get; set; } // Drinkname, e.g. Fanta, Cola
+        public double DrinkPrice  { get; set; } // DrinkPrice, e.g. Math, 3.99, 5.00
+        public int DrinkStock { get; set; } // DrinkStock, e.g. 20, 60
+        public double DrinkVAT { get; set; } // DrinkVAT, e.g. 21%
+    }
+}
