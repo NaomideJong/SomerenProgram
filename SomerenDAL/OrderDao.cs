8f0e24c15d403b683a0a1cd8d674d58db985972a
DaanTol <686379@student.inholland.nl>
2022-03-17 19:12:12 +0100

    Added coded comments

--- SomerenDAL/OrderDao.cs
+++ SomerenDAL/OrderDao.cs
@@ -13,45 +13,23 @@ namespace SomerenDAL
     public class OrderDao : BaseDao
     {
 
-        /*public List<Order> GetAllOrders()
-        {
-            string query = "SELECT orderId, studentId, drinkId FROM [Orders]";
-            SqlParameter[] sqlParameters = new SqlParameter[0];
-            return ReadTables(ExecuteSelectQuery(query, sqlParameters));
-        }*/
-
         public void AddOrder(Order order)
         {
+            // create a query where elements shall be inserted into the tables below
             string query = "INSERT INTO Orders (studentId, drinkId)" +
                 "VALUES (@studentId, @drinkId); " +
                 "SELECT SCOPE_IDENTITY();";
 
+            // get a new sqlParameter and put the value of order.StudentOrderId and DrinkOrderId
+            // in their respective tables
             SqlParameter[] sqlParameters = new SqlParameter[2]
             {
                 new SqlParameter("@studentId", order.StudentOrderId),
                 new SqlParameter("@drinkId", order.DrinkOrderId)
             };
 
+            // execute the query and the parameters
             ExecuteEditQuery(query, sqlParameters);
         }
-
-        /*private List<Order> ReadTables(DataTable dataTable)
-        {
-            List<Order> orders = new List<Order>();
-
-            // check each row of the DataTable
-            foreach (DataRow dr in dataTable.Rows)
-            {
-                Order order = new Order()
-                {
-                    OrderId = (int)dr["orderId"],
-                    StudentOrderId = (int)dr["studentId"],
-                    DrinkOrderId = (int)dr["drinkId"],
-                };
-                // add the order to the list
-                orders.Add(order);
-            }
-            return orders;
-        }*/
     }
 }
