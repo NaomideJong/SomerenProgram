9985a6160bb281444d4595477ea7b973517ed4a7
DaanTol <686379@student.inholland.nl>
2022-03-17 21:44:08 +0100

    Added a dayOfPurchase

--- SomerenModel/Order.cs
+++ SomerenModel/Order.cs
@@ -11,11 +11,13 @@ namespace SomerenModel
         public int OrderId { get; set; } // OrderId, e.g. 2
         public int StudentOrderId { get; set; } // StudentOrderId, e.g. 5, 7
         public int DrinkOrderId { get; set; } // DrinkOrderId, e.g. 1, 6
+        public DateTime DateOfPurchase { get; set; }
 
-        public Order(int studentOrderId, int drinkOrderId)
+        public Order(int studentOrderId, int drinkOrderId, DateTime dateOfPurchase)
         {
             StudentOrderId = studentOrderId;
             DrinkOrderId = drinkOrderId;
+            DateOfPurchase = dateOfPurchase;
         }
     }
 }
