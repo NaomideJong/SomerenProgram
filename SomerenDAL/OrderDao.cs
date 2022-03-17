06e3029e2c5c15683416ef01d006ff8ae0e79add
DaanTol <686379@student.inholland.nl>
2022-03-17 21:43:32 +0100

    Added a dateofpurchase to the paramaters

--- SomerenDAL/OrderDao.cs
+++ SomerenDAL/OrderDao.cs
@@ -16,16 +16,17 @@ namespace SomerenDAL
         public void AddOrder(Order order)
         {
             // create a query where elements shall be inserted into the tables below
-            string query = "INSERT INTO Orders (studentId, drinkId)" +
-                "VALUES (@studentId, @drinkId); " +
+            string query = "INSERT INTO Orders (studentId, drinkId, dateOfPurchase)" +
+                "VALUES (@studentId, @drinkId, @dateOfPurchase); " +
                 "SELECT SCOPE_IDENTITY();";
 
             // get a new sqlParameter and put the value of order.StudentOrderId and DrinkOrderId
             // in their respective tables
-            SqlParameter[] sqlParameters = new SqlParameter[2]
+            SqlParameter[] sqlParameters = new SqlParameter[3]
             {
                 new SqlParameter("@studentId", order.StudentOrderId),
-                new SqlParameter("@drinkId", order.DrinkOrderId)
+                new SqlParameter("@drinkId", order.DrinkOrderId),
+                new SqlParameter("dateOfPurchase", order.DateOfPurchase)
             };
 
             // execute the query and the parameters
