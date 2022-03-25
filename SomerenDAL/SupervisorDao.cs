267fc7299e22a801660636baa610ce11db5e28d7
NaomideJong <562748@student.inholland.nl>
2022-03-25 19:47:31 +0100

    added supervisor stuff

---
+++ SomerenDAL/SupervisorDao.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using SomerenModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SomerenDAL
+{
+    public class SupervisorDao : BaseDao
+    {
+        public void JoinTable()
+        {
+            string query = "SELECT Activity.*, Teacher.*" +
+                    "FROM Supervisors as ACS " +
+                    "JOIN Activities as Activity on ACS.activityId = Activity.activityId " +
+                    "JOIN Teachers as Teacher on ACS.teacherId = Teacher.teacherId " +
+                    "WHERE Activity.activityId = @activityId";
+            SqlParameter[] sqlParameters = new SqlParameter[0];
+            ExecuteSelectQuery(query, sqlParameters);
+        }
+    }
+}
---
+++ SomerenModel/Supervisor.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SomerenModel
+{
+    public class Supervisor
+    {
+        Teacher teacher = new Teacher();
+
+        public Supervisor(int teacherId, int activityId)
+        {
+            teacher.TeacherId = teacherSupervisingId;
+            ActivityId = activitySupervisedId;
+        }
+    }
+}
