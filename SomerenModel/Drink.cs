d90b25146899a6721f14a727fd4bb39490fa8ed4
DaanTol <686379@student.inholland.nl>
2022-03-25 19:34:53 +0100

    Added, change, add and delete button

--- SomerenModel/Drink.cs
+++ SomerenModel/Drink.cs
@@ -10,11 +10,19 @@ namespace SomerenModel
     {
         public int DrinkId { get; set; } // DrinkId, e.g. 2
         public string DrinkName { get; set; } // Drinkname, e.g. Fanta, Cola
-        public decimal DrinkPrice  { get; set; } // DrinkPrice, e.g. Math, 3.99, 5.00
+        public decimal DrinkPrice { get; set; } // DrinkPrice, e.g. Math, 3.99, 5.00
         public int DrinkStock { get; set; } // DrinkStock, e.g. 20, 60
         public int DrinkVAT { get; set; } // DrinkVAT, e.g. 21%
         public decimal DrinkValue { get; set; } //money earned from the drink probably?
         public int DrinksSold { get; set; } //amount of drinks sold
-        public bool StockAmount { get; set; }
+        public bool StockAmount
+        {
+            get
+            {
+                bool stock = true;
+                if (this.DrinkStock < 10) stock = false;
+                return stock;
+            }
+        }
     }
 }
