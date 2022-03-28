2662362b89f5ee840c6ce418d726bb9dab112d76
NaomideJong <562748@student.inholland.nl>
2022-03-28 14:23:22 +0200

    added login, logindao and loginservice

---
+++ SomerenDAL/LogInDao.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using SomerenModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SomerenDAL
+{
+    public class LogInDao : BaseDao
+    {
+        public LogIn GetById(string userId)
+        {
+            string query = $"SELECT userId, adminStatus, userPassword FROM [Users] " +
+                $"WHERE userId = @userId";
+            SqlParameter[] sqlParameters = new SqlParameter[1]
+            {
+                new SqlParameter("@userId", userId)
+            };
+
+            return ReadTable(ExecuteSelectQuery(query, sqlParameters))[0];
+        }
+        private List<LogIn> ReadTable(DataTable dataTable)
+        {
+            List<LogIn> logIns = new List<LogIn>();
+
+            // check each row of the DataTable
+            foreach (DataRow dr in dataTable.Rows)
+            {
+                LogIn logIn = new LogIn()
+                {
+                    UserId = (string)dr["userId"],
+                    AdminStatus = (string)dr["adminStatus"],
+                    UserPassword = (string)dr["userPassword"]
+                };
+                // add the drink to the list
+                logIns.Add(logIn);
+            }
+            return logIns;
+        }
+    }
+}
---
+++ SomerenLogic/LogInService.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using SomerenDAL;
+using SomerenModel;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SomerenLogic
+{
+    public class LogInService
+    {
+        LogInDao logIndb;
+
+        public LogInService()
+        {
+            logIndb = new LogInDao();
+        }
+
+        public LogIn GetById()
+        {
+
+        }
+    }
+}
